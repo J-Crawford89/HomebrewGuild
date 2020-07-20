b4e2dfd34e58a2ba753eb25245d0c9dbb3e6b8eb
J-Crawford89 <J.Crawford0120@Gmail.com>
2020-07-19 21:05:44 -0400

    removed delete views, added user and userandrole

---
+++ MVC/Controllers/UserAndRoleController.cs
@@ -0,0 +1,56 @@
+﻿using Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    [Authorize]
+    public class UserAndRoleController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged In";
+            }
+            return View();
+        }
+        public bool isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var userRoles = UserManager.GetRoles(user.GetUserId());
+                foreach (var role in userRoles)
+                {
+                    if (role.ToString() == "Admin")
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- MVC/Controllers/UserController.cs
+++ MVC/Controllers/UserController.cs
@@ -1,7 +1,4 @@
-﻿using Data;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,48 +6,12 @@ using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
-    [Authorize]
     public class UserController : Controller
     {
-        // GET: Users
+        // GET: User
         public ActionResult Index()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                var user = User.Identity;
-                ViewBag.Name = user.Name;
-
-                ViewBag.displayMenu = "No";
-
-                if (isAdminUser())
-                {
-                    ViewBag.displayMenu = "Yes";
-                }
-                return View();
-            }
-            else
-            {
-                ViewBag.Name = "Not Logged In";
-            }
             return View();
-        }
-        public bool isAdminUser()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                var user = User.Identity;
-                ApplicationDbContext context = new ApplicationDbContext();
-                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-                var userRoles = UserManager.GetRoles(user.GetUserId());
-                foreach (var role in userRoles)
-                {
-                    if (role.ToString() == "Admin")
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
         }
     }
 }
