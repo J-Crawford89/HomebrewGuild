e6f06045ab49db121f4f503fa8db624d5cecf82a
J-Crawford89 <J.Crawford0120@Gmail.com>
2020-08-14 14:58:29 -0400

    fixed user links, spell creation validation

--- Models/SpellModels/SpellCreate.cs
+++ Models/SpellModels/SpellCreate.cs
@@ -11,7 +11,9 @@ namespace Models.SpellModels
 {
     public class SpellCreate
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         [Display(Name="Spell Level")]
         public int SpellLevel { get; set; }
         public School School { get; set; }
@@ -20,13 +22,17 @@ namespace Models.SpellModels
         [Display(Name ="Concentration")]
         public bool RequiresConcentration { get; set; }
         [Display(Name="Casting Time")]
+        [Required]
         public string CastingTime { get; set; }
+        [Required]
         public string Range { get; set; }
         [Display(Name="Components")]
         public List<SpellComponent> Components { get; set; } = new List<SpellComponent>();
         [Display(Name="Material Component")]
         public string MaterialComponent { get; set; }
+        [Required]
         public string Duration { get; set; }
+        [Required]
         public string Description { get; set; }
         [Display(Name ="Class Spell Lists")]
         public ICollection<int> ClassIds { get; set; }
